cc35f428638401506ace6b9e63b2616583806e54
victorlin12345 <victorlin12345@gmail.com>
2019-09-06 17:04:49 +0800

    Add http func to show data

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"encoding/xml"
-	"fmt"
 	"io/ioutil"
 	"net/http"
+	"text/template"
 )
 
 var INDEX_URL string = "https://www.bnext.com.tw/sitemap/google"
 
-func main() {
+// Struct for Showing Data
+type BNewsAggPage struct {
+	PageTile string
+	News     map[string]Article
+}
+
+func bnewsAggHandler(w http.ResponseWriter, r *http.Request) {
 
 	var smi SitemapIndex
 	smi.FeedData(INDEX_URL)
@@ -17,22 +23,31 @@ func main() {
 	article_map := make(map[string]Article)
 
 	for _, Loc := range smi.GetLocations() {
+
 		var articles Articles
 		resp, _ := http.Get(Loc)
 		bytes, _ := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
 		xml.Unmarshal(bytes, &articles)
+
 		for idx, _ := range articles.Locations {
+
 			a := Article{Location: articles.Locations[idx],
 				PucbicationDate: articles.PucbicationDates[idx]}
+
 			article_map[articles.Titles[idx]] = a
 		}
 	}
 
-	for t, a := range article_map {
-		fmt.Printf("\n\n\n%s", t)
-		fmt.Printf("\n%s", a.Location)
-		fmt.Printf("\n%s", a.PucbicationDate)
-	}
+	p := BNewsAggPage{PageTile: "數位雜誌文章", News: article_map}
+	t, _ := template.ParseFiles("agg.html")
+	t.Execute(w, p)
+
+}
+
+func main() {
+
+	http.HandleFunc("/", bnewsAggHandler)
+	http.ListenAndServe(":8000", nil)
 
 }
