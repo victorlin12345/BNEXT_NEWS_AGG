a6405d5f0f7a2f47b5761f19a4e9bf3fd97f538b
victorlin12345 <victorlin12345@gmail.com>
2019-09-07 17:59:29 +0800

    Change to concurrency

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/xml"
 	"io/ioutil"
 	"net/http"
+	"sort"
+	"sync"
 	"text/template"
 )
 
@@ -15,30 +17,59 @@ type BNewsAggPage struct {
 	News     []Article
 }
 
+var wg sync.WaitGroup
+
+func newsRoutine(c chan Articles, Location string) {
+	defer wg.Done()
+	var as Articles
+	resp, _ := http.Get(Location)
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &as)
+	resp.Body.Close()
+	c <- as
+}
+
 func bnewsAggHandler(w http.ResponseWriter, r *http.Request) {
 
 	var smi SitemapIndex
 	smi.FeedData(INDEX_URL)
 
 	var showlist []Article
+	var queue chan Articles = make(chan Articles, 100)
 
-	for _, Loc := range smi.GetLocations(5) {
+	for _, Loc := range smi.GetLocations(10) {
+		wg.Add(1)
+		go newsRoutine(queue, Loc)
 
-		var articles Articles
-		resp, _ := http.Get(Loc)
-		bytes, _ := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		xml.Unmarshal(bytes, &articles)
+		// var as Articles
+		// resp, _ := http.Get(Loc)
+		// bytes, _ := ioutil.ReadAll(resp.Body)
+		// xml.Unmarshal(bytes, &as)
+		// resp.Body.Close()
 
-		for idx, _ := range articles.Locations {
+		// for idx, _ := range as.Titles {
+		// 	a := Article{Title: as.Titles[idx],
+		// 		Location:        as.Locations[idx],
+		// 		PucbicationDate: as.PucbicationDates[idx]}
+		// 	showlist = append(showlist, a)
+		// }
+	}
 
-			a := Article{Title: articles.Titles[idx],
-				Location:        articles.Locations[idx],
-				PucbicationDate: articles.PucbicationDates[idx]}
+	wg.Wait()
+	close(queue)
 
+	for as := range queue {
+		for idx, _ := range as.Titles {
+			a := Article{Title: as.Titles[idx],
+				Location:        as.Locations[idx],
+				PucbicationDate: as.PucbicationDates[idx]}
 			showlist = append(showlist, a)
 		}
 	}
+
+	sort.SliceStable(showlist, func(i, j int) bool {
+		return showlist[i].PucbicationDate > showlist[j].PucbicationDate
+	})
 
 	p := BNewsAggPage{PageTile: "數位雜誌文章", News: showlist}
 	t, _ := template.ParseFiles("agg.html")
